65926d8960c8084813254cafaeaf0a8464683a73
Matthew <matthew.d.baker20@gmail.com>
2021-01-10 15:58:13 -0600

    weather api call added

--- src/WeatherArea/WeatherArea.js
+++ src/WeatherArea/WeatherArea.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import WeatherCard from "../WeatherCards/WeatherCard";
+import openWeatherApi from "../weatherKey";
 
 const WeatherArea = () => {
+
+    const lng = -98.49;
+    const lat = 29.42;
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=hourly,minutely&units=imperial&appid=${openWeatherApi}`).then( r => {
+        r.json().then(data => console.log(data));
+    });
 
     return (
         <div id="weather">
