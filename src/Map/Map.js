26f30d7b8f86931591ee8d2fde7444b7534384ff
Matthew <matthew.d.baker20@gmail.com>
2021-01-10 15:42:43 -0600

    mapbox on site

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import mapboxgl from 'mapbox-gl';
+import mapboxToken from "../keys";
 
-const Map = () => {
+mapboxgl.accessToken = mapboxToken;
+
+class Map extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: -97.7,
+            lat: 31.3,
+            zoom: 5
+        }
+    }
 
 
-    return (
-        <div id="mapBoxArea">
-            <p>Map here</p>
-        </div>
-    )
 
+
+    componentDidMount() {
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-97.7, 31.3],
+            zoom: 9
+        });
+
+    }
+
+
+
+
+    render() {
+        return (
+            <div id="mapBoxArea">
+                <div ref={el => this.mapContainer = el} className="mapContainer"/>
+            </div>
+        )
+    }
 }
 
 export default Map;
